a7050ed87c17ed36937b05c9d51b1643443eb7cc
Shaban Karumba <shabankarumba@gmail.com>
2020-04-13 19:23:41 +0100

    Only save availabilities that have dates in the future

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,4 +1,11 @@
 class Availability < ApplicationRecord
+  belongs_to :user
+
   validates :available_at, :user_id, presence: true
   validates :available_at, uniqueness: { scope: :user_id }
+  validate :available_at_not_in_the_past
+
+  def available_at_not_in_the_past
+    errors.add(:available_at, 'Availability should be in the future') if available_at.past?
+  end
 end
