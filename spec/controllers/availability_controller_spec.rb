431cbf5df357b1a9d019202bdd9a015de20cf91f
Shaban Karumba <shabankarumba@gmail.com>
2020-10-17 19:23:54 +0100

    Remove redundant spec

--- spec/controllers/availability_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AvailabilityController, type: :controller do
-  describe '#create' do
-    it 'redirects if the user is not signed in' do
-
-    end
-  end
-end
