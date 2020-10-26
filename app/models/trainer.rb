1142ca7274bee9cc3c8951c42dd6b5b5fa1edcc3
Shaban Karumba <shabankarumba@gmail.com>
2020-10-26 09:27:40 +0000

    Hide trainers if they don't have a first_name or last_name

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,4 +2,10 @@ class Trainer < User
   has_many :availabilities, foreign_key: :user_id
   has_many :purchased_sessions
   has_many :sessions, through: :purchased_sessions
+
+  scope :active, -> do
+    joins(:profile)
+      .where.not(first_name: nil)
+      .where.not(last_name: nil)
+  end
 end
