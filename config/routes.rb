4085dda142e6fafcf1b932d2eff41049cb1fe200
Shaban Karumba <shabankarumba@gmail.com>
2020-10-29 23:41:21 +0000

    Add terms & conditions link to the footer of the website pages

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :availability, only: [:index, :create]
   get '/twilio/token', action: :token, controller: 'twilio'
   resources :rooms, only: [:show]
+  get '/terms-and-conditions', action: :index, controller: 'terms_and_conditions'
 end
