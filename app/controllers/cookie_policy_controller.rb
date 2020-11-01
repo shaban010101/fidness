cff0a1c83e5a15f05501e2d98e079db69a68dc3b
Shaban Karumba <shabankarumba@gmail.com>
2020-11-01 14:32:51 +0000

    Fix coding style issue with empty method

--- app/controllers/cookie_policy_controller.rb
+++ app/controllers/cookie_policy_controller.rb
@@ -1,5 +1,3 @@
 class CookiePolicyController < ApplicationController
-  def index
-
-  end
+  def index; end
 end
--- app/controllers/terms_and_conditions_controller.rb
+++ app/controllers/terms_and_conditions_controller.rb
@@ -1,5 +1,3 @@
 class TermsAndConditionsController < ApplicationController
-  def index
-
-  end
+  def index; end
 end
