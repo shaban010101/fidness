3f71052812a778b25daa7fca98b58450f2d9c2e6
Shaban Karumba <shabankarumba@gmail.com>
2020-11-10 07:22:53 +0000

    Use jquery rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery/dist/jquery.min.js
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require modernizr-2.6.2.min.js
 //= require main
